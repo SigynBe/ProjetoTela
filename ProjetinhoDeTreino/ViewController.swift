309507508b723f0b6d098b8da3c804ef2e3d2e53
Rebeca PachecoCorrea <rebecapc@icomp.ufam.edu.br>
2020-06-29 09:55:15 -0400

    Initial Commit

---
+++ ProjetinhoDeTreino/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjetinhoDeTreino
+//
+//  Created by Rebeca Pacheco on 29/06/20.
+//  Copyright © 2020 Rebeca Pacheco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
